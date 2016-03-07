e87bfbe02020346a1b83421f29c87b1156332f2b
METTER-ROTHAN Jérémie <jeremie.metter@orange.fr>
2016-03-07 22:41:29 +0100

    Fonctions maths

--- platformer/javascript/utility.js
+++ platformer/javascript/utility.js
@@ -14,46 +14,44 @@ Array.inArray = function(needle){
     return -1;
 }
 
-platformer.Math = function(){
-    /**
-    * Arrondir un nombre à x décimale après la virgule
-    * a - chiffre à Arrondir
-    * b - nombre de décimale après la virgule
-    */
-    this.roundAt = function(a, b){
-        if(typeof b === 'undefined') b = 0;
-        var x = Math.pow(10, b);
-        return Math.round(a * x) / x;
-    }
+/**
+* Arrondir un nombre à x décimale après la virgule
+* a - chiffre à Arrondir
+* b - nombre de décimale après la virgule
+*/
+platformer.math.roundAt = function(a, b){
+    if(typeof b === 'undefined') b = 0;
+    var x = Math.pow(10, b);
+    return Math.round(a * x) / x;
+}
 
-    /**
-    * Interpolation
-    */
-    this.lerp = function(start, end, percent){
-         return (start + percent*(end - start));
-    }
+/**
+* Interpolation
+*/
+platformer.math.lerp = function(start, end, percent){
+     return (start + percent*(end - start));
+}
 
-    /**
-    * Random
-    */
-    this.randomInt = function(min, max){
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
+/**
+* Random
+*/
+platformer.math.randomInt = function(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-    this.randomArbitrary = function(min, max){
-        return Math.random() * (max - min) + min;
-    }
+platformer.math.randomArbitrary = function(min, max){
+    return Math.random() * (max - min) + min;
+}
 
-    this.randomFloat = function(min, max){
-        return Math.round(this.randomArbitrary(min, max) * 100) / 100;
-    }
+platformer.math.randomFloat = function(min, max){
+    return Math.round(platformer.math.randomArbitrary(min, max) * 100) / 100;
+}
 
-    this.randomChoiceArray = function(choices){
-        return choices[getRandomInt(0, choices.length - 1)];
-    }
+platformer.math.randomChoiceArray = function(choices){
+    return choices[platformer.math.randomInt(0, choices.length - 1)];
+}
 
-    this.randomChoiceObject = function(obj){
-        var keys = Object.keys(obj);
-        return obj[keys[getRandomInt(0, keys.length)]];
-    }
+platformer.math.randomChoiceObject = function(obj){
+    var keys = Object.keys(obj);
+    return obj[keys[platformer.math.randomInt(0, keys.length)]];
 }
