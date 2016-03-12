2ee1db568ab4ed4f636d239b678f3e4cbbd3aeb8
METTER-ROTHAN Jérémie <jeremie.metter@orange.fr>
2016-03-12 20:23:56 +0100

    Fonctions utiles

--- platformer/javascript/utility.js
+++ platformer/javascript/utility.js
@@ -19,7 +19,7 @@ Array.inArray = function(needle){
 * a - chiffre à Arrondir
 * b - nombre de décimale après la virgule
 */
-platformer.math.roundAt = function(a, b){
+function roundAt(a, b){
     if(typeof b === 'undefined') b = 0;
     var x = Math.pow(10, b);
     return Math.round(a * x) / x;
@@ -28,41 +28,41 @@ platformer.math.roundAt = function(a, b){
 /**
 * Interpolation
 */
-platformer.math.lerp = function(start, end, percent){
+function lerp(start, end, percent){
      return (start + percent*(end - start));
 }
 
 /**
 * Random
 */
-platformer.math.randomInt = function(min, max){
+function randomInt(min, max){
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-platformer.math.randomArbitrary = function(min, max){
+function randomArbitrary(min, max){
     return Math.random() * (max - min) + min;
 }
 
-platformer.math.randomFloat = function(min, max){
-    return Math.round(platformer.math.randomArbitrary(min, max) * 100) / 100;
+function randomFloat(min, max){
+    return Math.round(randomArbitrary(min, max) * 100) / 100;
 }
 
-platformer.math.randomChoiceArray = function(choices){
-    return choices[platformer.math.randomInt(0, choices.length - 1)];
+function randomChoiceArray(choices){
+    return choices[randomInt(0, choices.length - 1)];
 }
 
-platformer.math.randomChoiceObject = function(obj){
+function randomChoiceObject(obj){
     var keys = Object.keys(obj);
-    return obj[keys[platformer.math.randomInt(0, keys.length)]];
+    return obj[keys[randomInt(0, keys.length)]];
 }
 
-platformer.math.toFloat = function(a){
+function toFloat(a){
     return Math.floor(a * 100) / 100;
 }
 
 /**
 * Retourne la timestamp courante
 */
-platformer.timestamp = function(){
+function timestamp(){
     return window.performance && window.performance.now ? window.performance.now() : new Date().getTime();
 }
