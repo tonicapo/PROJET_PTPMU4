cc5e74380dc87fec388734c9923e0bc72a61fbe9
METTER-ROTHAN Jérémie <jeremie.metter@orange.fr>
2016-03-18 09:47:31 +0100

    Ajout de la classe Effet

--- platformer/javascript/objects/effects/Effect.js
+++ platformer/javascript/objects/effects/Effect.js
@@ -1,5 +1,28 @@
-function Effect(position){
-    Rectangle(this, position.x, position.y, width, height);
+function Effect(level, animation, position, width, height){
+    Rectangle.call(this, position.x, position.y, width, height);
 
-    
+    var self = this;
+    this.setRenderBox(platformer.tileSizeX, platformer.tileSizeY);
+    this.setFixedToBottom(false);
+
+    level.getTimers().addTimer(function(){
+        self.setDirty(true);
+    }, animation.getSpeed());
+
+    this.init = function(){
+        animation.init();
+    }
+
+    this.render = function(ctx, panX, panY){
+        var renderBox = this.getRenderBox();
+        ctx.drawImage(animation.getFrame(), renderBox.x - panX, renderBox.y - panY, renderBox.width, renderBox.height);
+    }
+
+    this.update = function(){
+        animation.update();
+    }
+}
+
+function FireBallExplosion(level, position){
+    Effect.call(this, level, new Animation('explosion', platformer.textures.effects.fireBallExploding, 200, { cancelable : false }), position, platformer.tileSizeX, platformer.tileSizeY);
 }
