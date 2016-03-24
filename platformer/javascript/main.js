ab1bcb1f78b9bae2256f84da8fc20fc41e32d68f
METTER-ROTHAN Jérémie <jeremie.metter@orange.fr>
2016-03-24 22:55:48 +0100

    agencement du code

--- platformer/javascript/main.js
+++ platformer/javascript/main.js
@@ -1,3 +1,15 @@
 window.addEventListener('DOMContentLoaded', function(){
-    platformer.init();
+    platformer.init('bloodAndGuts', {
+        title : 'Blood & Guts',
+        debug : true,
+
+        width : 1440,
+        height : 700,
+
+        fullscreen : false,
+
+        seed : 'caca',
+
+        font : 'silkscreen'
+    });
 });
